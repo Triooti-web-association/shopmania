0a17152639138109c066f2972f454a158fde77fc
unknown <mytriooti@gmail.com>
2022-12-11 17:14:16 +0000

    deploiement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,8 @@ export const environment = {
 //  apiBaseUrl: 'https://businesse-server.herokuapp.com/shop-mania/v1'
 //  apiBaseUrl: 'http://localhost:8080/dpshop-backend-0.0.1-SNAPSHOT/shop-mania/v1'
 
- // apiBaseUrl: 'https://businesse-admin.herokuapp.com/shop-mania/v1'
+apiBaseUrl: 'https://dpshop.de1.mondomainegratuit.com/dpshop/shop-mania/v1'
+//  apiBaseUrl: 'https://businesse-admin.herokuapp.com/shop-mania/v1'
+ // apiBaseUrl: 'http://62.171.128.8:8080/dpshop-backend-0.0.1-SNAPSHOT/shop-mania/v1'
+
 };
